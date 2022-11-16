ef507b8e2bb8e04513308ea76910d598ae1769c7
MarceloJon <mrojasl@pucp.edu.pe>
2022-11-15 20:44:15 -0500

    base gaaa

---
+++ src/main/java/com/example/lab9grupof/Lab9grupoFApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab9grupof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab9grupoFApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab9grupoFApplication.class, args);
+    }
+
+}
